d5bf3d65e482dac5e4fb089f4bd9d4d9d023330b
Diego Camarillo <bigocam123@gmail.com>
2025-04-30 10:10:44 -0600

    Limpiando

--- routes/productos.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const verificarAutenticacion = require('../middlewares/authMiddleware');
-const { crearProducto, obtenerProductos } = require('../controllers/productosController');
-
-router.post('/', verificarAutenticacion, crearProducto);
-router.get('/', obtenerProductos);
-
-module.exports = router;
